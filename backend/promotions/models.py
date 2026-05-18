303427e8b2e354c99f369c1f581491c3c3393afd
Walid <walidetliam@gmail.com>
2026-05-18 11:01:14 +0200

    Ajout modèles pédagogiques

--- backend/promotions/models.py
+++ backend/promotions/models.py
@@ -1,9 +1,48 @@
 from django.db import models
-
-
-
 from django.contrib.auth.models import User
 
+class Filiere(models.Model):
+
+    nom = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+
+        return self.nom
+
+class Cursus(models.Model):
+
+    nom = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    Filiere = models.ForeignKey(Filiere, on_delete=models.CASCADE, related_name='cursus')
+
+    def __str__(self):
+
+        return self.nom
+class Cours(models.Model):
+
+    titre = models.CharField(max_length=255)
+    technologie = models.CharField(max_length=100, blank=True)
+    description = models.TextField(blank=True)
+    
+    def __str__(self):
+
+        return self.nom      
+
+class CursusCours(models.Model):
+
+    cursus = models.ForeignKey(Cursus, on_delete=models.CASCADE, related_name='cursus_cours')
+    cours = models.ForeignKey(Cours, on_delete=models.CASCADE)
+    ordre = models.PositiveIntegerField()
+    obligatoire = models.BooleanField(default=True)
+
+    class Meta:
+
+        unique_together = ('cursus', 'cours')
+        constraints = [
+           models.UniqueConstraint(fields=['cursus', 'ordre'], name='unique_cursus_ordre')
+        ]
+        ordering = ['ordre']
 
 
 class Promotion(models.Model):
@@ -83,3 +122,4 @@ class Inscription(models.Model):
 
 
         return f"{self.utilisateur.username} - {self.promotion.nom}"
+
