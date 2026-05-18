c5df90198186176aaa44cfec15e683e3d632d49d
Laure-Anne Vincent-Aponte <laureanne.vincent@gmail.com>
2026-05-18 11:02:01 +0200

    Revert "Moved project's secret key to .env"
    This reverts commit d9acf4d0818bc726dea9dc3b22727cfaad9deb34.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -23,7 +23,7 @@ load_dotenv(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = "django-insecure-0yk%6^%1xl%+wab0l#9(7-tk^ps%_&)#2jo5wpao)eadu52^&4"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
