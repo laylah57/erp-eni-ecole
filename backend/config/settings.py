34be323446cb5a487fa8b24891c825346e8b8d20
Laure-Anne Vincent-Aponte <laureanne.vincent@gmail.com>
2026-05-18 09:36:59 +0200

    Added Python Code formatter configuration

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -16,7 +16,7 @@ from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR/ '.env')
+load_dotenv(BASE_DIR / ".env")
 
 
 # Quick-start development settings - unsuitable for production
@@ -80,11 +80,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv('DB_NAME'),
-        "USER": os.getenv('DB_USER'),
-        "PASSWORD": os.getenv('DB_PASSWORD'),
-        "HOST": os.getenv('DB_HOST'),
-        "PORT": os.getenv('DB_PORT')
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
--- backend/promotions/models.py
+++ backend/promotions/models.py
@@ -1,85 +1,34 @@
 from django.db import models
 
 
-
 from django.contrib.auth.models import User
-
 
 
 class Promotion(models.Model):
 
-
-
     nom = models.CharField(max_length=100)
-
-
 
     date_debut = models.DateField()
 
-
-
     date_fin = models.DateField()
 
-
-
     def __str__(self):
-
-
 
         return self.nom
 
 
-
 class Inscription(models.Model):
 
-
-
-    utilisateur = models.ForeignKey(
-
-
-
-        User,
-
-
-
-        on_delete=models.CASCADE
-
-
-
-    )
-
-
+    utilisateur = models.ForeignKey(User, on_delete=models.CASCADE)
 
     promotion = models.ForeignKey(
-
-
-
-        Promotion,
-
-
-
-        on_delete=models.CASCADE,
-
-
-
-        related_name='inscriptions'
-
-
-
+        Promotion, on_delete=models.CASCADE, related_name="inscriptions"
     )
-
-
 
     date_inscription = models.DateField()
 
-
-
     forcee = models.BooleanField(default=False)
 
-
-
     def __str__(self):
-
-
 
         return f"{self.utilisateur.username} - {self.promotion.nom}"
