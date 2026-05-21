5341932eb62574142c6c16c94119fa4dd9179666
Laure-Anne Vincent-Aponte <laureanne.vincent@gmail.com>
2026-05-21 12:00:54 +0200

    Added auth services and connected back to front

--- frontend/src/app/components/login/login.ts
+++ frontend/src/app/components/login/login.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
-import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
-import {Auth} from '../../services/auth';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'app-login',
@@ -9,30 +10,32 @@ import {Auth} from '../../services/auth';
   styleUrl: './login.css',
 })
 export class Login {
-  private auth = inject(Auth)
+  private auth = inject(Auth);
 
   loginForm = new FormGroup({
     email: new FormControl("", [Validators.required, Validators.email]), // Decide what should be the minimum length
     // of the username
-    password: new FormControl("", [Validators.required, Validators.minLength(6)])
-  })
+    password: new FormControl("", [Validators.required, Validators.minLength(6)]),
+  });
 
   onSubmit() {
-    if(this.loginForm.invalid) {
-      this.loginForm.markAllAsTouched() // Errors appear on all field if submit button clicked while invalid
-      return
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched(); // Errors appear on all field if submit button clicked while invalid
+      return;
     } else {
-      const email = this.loginForm.controls.email.value ?? '' // Value ?? empty string for type reasons.
-      const password = this.loginForm.controls.password.value ?? ''
+      const email = this.loginForm.controls.email.value ?? ''; // Value ?? empty string for type reasons.
+      const password = this.loginForm.controls.password.value ?? '';
 
       this.auth.login(email, password).subscribe({
         next: (tokens) => {
-          console.log(tokens)
+          this.auth.saveTokens(tokens);
+          this.auth.getCurrentUser().subscribe({
+            next: (user) => console.log(user),
+            error: (error) => console.error(error),
+          });
         },
-        error: (error) => {
-          console.error(error)
-        }
-      })
+        error: (error) => console.error(error),
+      });
     }
   }
 }
--- frontend/src/app/services/auth.ts
+++ frontend/src/app/services/auth.ts
@@ -1,22 +1,61 @@
-import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
 
-interface Token {
+interface AuthTokens {
   access: string;
-  refresh: string
+  refresh: string;
+}
+
+interface CurrentUser {
+  id: number;
+  username: string;
+  email: string;
+  first_name: string | null;
+  last_name: string | null;
+  groups: string[];
 }
 
 @Injectable({
   providedIn: 'root',
 })
 export class Auth {
-  private http = inject(HttpClient)
-  private apiUrl = 'http://127.0.0.1:8000/api'
+  private http = inject(HttpClient);
+  private apiUrl = 'http://127.0.0.1:8000/api';
 
   login(email: string, password: string) {
-    return this.http.post<Token>(`${this.apiUrl}/token/`, {
+    return this.http.post<AuthTokens>(`${this.apiUrl}/token/`, {
       email,
-      password
-    })
+      password,
+    });
+  }
+
+  saveTokens(tokens: AuthTokens) {
+    localStorage.setItem('access_token', tokens.access);
+    localStorage.setItem('refresh_token', tokens.refresh);
+  }
+
+  getAccessToken() {
+    return localStorage.getItem('access_token');
+  }
+
+  getRefreshToken() {
+    return localStorage.getItem('refresh_token');
+  }
+
+  logout() {
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('refresh_token');
+  }
+
+  getCurrentUser() {
+    const accessToken = this.getAccessToken();
+    if (!accessToken) {
+      throw new Error('The access token does not exist');
+    }
+    return this.http.get<CurrentUser>(`${this.apiUrl}/accounts/me/`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`, // Eventually, use Angular HTTP interceptor
+      },
+    });
   }
 }
