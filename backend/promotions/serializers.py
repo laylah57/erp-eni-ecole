9d182fcc8de082aa48b0529c1642972efa103b1a
Walid <walidetliam@gmail.com>
2026-05-22 10:23:07 +0200

    Ajout endpoints API pédagogie

--- backend/promotions/serializers.py
+++ backend/promotions/serializers.py
@@ -4,29 +4,57 @@ from .models import (
     Promotion,
     Inscription,
     CoursPlanifie,
-    Animer
+    Animer,
+    Filiere,
+    Cursus,
+    Cours,
+    CursusCours,
 )
 
 
 class PromotionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Promotion
-        fields = '__all__'
+        fields = "__all__"
 
 
 class InscriptionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Inscription
-        fields = '__all__'
+        fields = "__all__"
 
 
 class CoursPlanifieSerializer(serializers.ModelSerializer):
     class Meta:
         model = CoursPlanifie
-        fields = '__all__'
+        fields = "__all__"
 
 
 class AnimerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Animer
-        fields = '__all__'
+        fields = "__all__"
+
+
+class FiliereSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Filiere
+        fields = "__all__"
+
+
+class CursusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cursus
+        fields = "__all__"
+
+
+class CoursSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cours
+        fields = "__all__"
+
+
+class CursusCoursSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CursusCours
+        fields = "__all__"
